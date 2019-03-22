cde5d670e11a55878a79fcd0535802c89f012587
dcarbajal <daniel.carbajal@pucp.edu.pe>
2019-03-22 09:29:01 -0500

    First commit

---
+++ Presentation.Service/Jobs/FileCreationJob.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf.Logging;
+
+namespace Presentation.Service.Jobs
+{
+    class FileCreationJob : IJob
+    {
+        private static readonly LogWriter _log = HostLogger.Get<FileCreationJob>();
+
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                string now = DateTime.Now.ToString("mmddyyyyhhmmss");
+                _log.Info($"File {now}.txt was created");
+                File.AppendAllLines($@"C:\Files\{now}.txt", new[] { "Hellow world from Lima, Peru" });
+                
+            }
+            catch(Exception exception)
+            {
+                _log.Error(exception);
+            }
+        }
+    }
+}
---
+++ Presentation.Service/Program.cs
@@ -0,0 +1,37 @@
+﻿using Presentation.Service.Jobs;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Quartz;
+
+namespace Presentation.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfiguration => {
+                serviceConfiguration.UseNLog();
+                serviceConfiguration.ScheduleQuartzJobAsService(service => {
+                    service.WithJob(() => JobBuilder.Create<FileCreationJob>().Build())
+                           .AddTrigger(() => TriggerBuilder.Create()
+                                            .WithSimpleSchedule(action =>
+                                                action.WithIntervalInSeconds(5).RepeatForever())
+                                            .Build());
+                });
+                serviceConfiguration.StartAutomatically();
+                serviceConfiguration.EnableServiceRecovery(recoveryOptions =>
+                {
+                    recoveryOptions.RestartService(1);
+                });
+                serviceConfiguration.SetDisplayName("File creator Service");
+                serviceConfiguration.SetDescription("Creates a file for the lulz");
+                serviceConfiguration.SetServiceName("File genesis");
+            });
+        }
+    }
+}
